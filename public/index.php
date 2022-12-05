1c73f4fa30fd90ba0d1cd5d97889ad49551957a0
Muhammad Irfan Lutfi <iluthfi469@gmail.com>
2022-12-05 13:46:01 +0700

    selesai

--- public/index.php
+++ public/index.php
@@ -4,8 +4,9 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use irfan\learning\php\mvc\App\Router;
+use irfan\learning\php\mvc\Controller\HomeController;
 
-Router::add('GET', '/', 'HomeController', 'index');
-Router::add('GET', '/login', 'UserController', 'login');
+Router::add('GET', '/', HomeController::class, 'index');
+Router::add('GET', '/login',  HomeController::class, 'login');
 
 Router::run();
